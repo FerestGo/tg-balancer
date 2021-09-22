6102774fe9853918432a1b94a3b4a1c2d1eb1943
Ilya Koshkarov <ferestgo@gmail.com>
2021-09-22 18:10:34 +0300

    change output

--- pkg/balancer/output.go
+++ pkg/balancer/output.go
@@ -11,35 +11,35 @@ func output(portfolio *Portfolio) string {
 	output += printer.Sprintf("Всего средств: %.2f руб. \n\n", portfolio.Total)
 
 	output += printer.Sprintf("Инструменты: \n")
-	output += printer.Sprintf("Акции %.0f%% (%.0f р.) \n", portfolio.PercentType.Stock, portfolio.Types.Stock)
-	output += printer.Sprintf("Облигации %.0f%% (%.0f р.) \n", portfolio.PercentType.Bonds, portfolio.Types.Bonds)
-	output += printer.Sprintf("Валюта %.0f%% (%.0f р.) \n", portfolio.PercentType.Currency, portfolio.Types.Currency)
-	output += printer.Sprintf("Биржевые товары %.0f%% (%.0f р.) \n", portfolio.PercentType.Goods, portfolio.Types.Goods)
+	output += printer.Sprintf("Акции %.1f%% (%.1f р.) \n", portfolio.PercentType.Stock, portfolio.Types.Stock)
+	output += printer.Sprintf("Облигации %.1f%% (%.1f р.) \n", portfolio.PercentType.Bonds, portfolio.Types.Bonds)
+	output += printer.Sprintf("Валюта %.1f%% (%.1f р.) \n", portfolio.PercentType.Currency, portfolio.Types.Currency)
+	output += printer.Sprintf("Биржевые товары %.1f%% (%.1f р.) \n", portfolio.PercentType.Goods, portfolio.Types.Goods)
 	if portfolio.PercentType.UndefinedEtf > 0 {
-		output += printer.Sprintf("Неизвестные ETF %.0f%% (%.0f р.) \n", portfolio.PercentType.UndefinedEtf, portfolio.Types.UndefinedEtf)
+		output += printer.Sprintf("Неизвестные ETF %.1f%% (%.1f р.) \n", portfolio.PercentType.UndefinedEtf, portfolio.Types.UndefinedEtf)
 	}
 
 	output += printer.Sprintf("\nВалюты: \n")
-	output += printer.Sprintf("RUB %.0f%% (%.0f р.) \n", portfolio.PercentCurrency.RUB, portfolio.Currency.RUB)
-	output += printer.Sprintf("USD %.0f%% (%.0f р.) \n", portfolio.PercentCurrency.USD, portfolio.Currency.USD)
-	output += printer.Sprintf("EUR %.0f%% (%.0f р.) \n\n", portfolio.PercentCurrency.EUR, portfolio.Currency.EUR)
+	output += printer.Sprintf("RUB %.1f%% (%.1f р.) \n", portfolio.PercentCurrency.RUB, portfolio.Currency.RUB)
+	output += printer.Sprintf("USD %.1f%% (%.1f р.) \n", portfolio.PercentCurrency.USD, portfolio.Currency.USD)
+	output += printer.Sprintf("EUR %.1f%% (%.1f р.) \n\n", portfolio.PercentCurrency.EUR, portfolio.Currency.EUR)
 
 	kv := mapToSortedSlice(portfolio.StockGeography.Country)
 	output += printer.Sprintf("Страны: \n")
 	for i := 0; i < len(kv); i++ {
-		output += printer.Sprintf("%s %.0f%% (%.0f р.) \n", kv[i].Key, portfolio.PercentStockGeography.Country[kv[i].Key], kv[i].Value)
+		output += printer.Sprintf("%s %.1f%% (%.1f р.) \n", kv[i].Key, portfolio.PercentStockGeography.Country[kv[i].Key], kv[i].Value)
 	}
 
 	kv = mapToSortedSlice(portfolio.StockGeography.Area)
 	output += printer.Sprintf("\nЗоны: \n")
 	for i := 0; i < len(kv); i++ {
-		output += printer.Sprintf("%s %.0f%% (%.0f р.) \n", kv[i].Key, portfolio.PercentStockGeography.Area[kv[i].Key], kv[i].Value)
+		output += printer.Sprintf("%s %.1f%% (%.1f р.) \n", kv[i].Key, portfolio.PercentStockGeography.Area[kv[i].Key], kv[i].Value)
 	}
 
 	kv = mapToSortedSlice(portfolio.StockGeography.MarketType)
 	output += printer.Sprintf("\nРынки: \n")
 	for i := 0; i < len(kv); i++ {
-		output += printer.Sprintf("%s %.0f%% (%.0f р.) \n", kv[i].Key, portfolio.PercentStockGeography.MarketType[kv[i].Key], kv[i].Value)
+		output += printer.Sprintf("%s %.1f%% (%.1f р.) \n", kv[i].Key, portfolio.PercentStockGeography.MarketType[kv[i].Key], kv[i].Value)
 	}
 
 	return output
