1e6f0b54517756240c1a687be80381d4a58158d0
Ilya Koshkarov <ferestgo@gmail.com>
2021-10-08 20:04:30 +0300

    update token error

--- pkg/balancer/main.go
+++ pkg/balancer/main.go
@@ -39,8 +39,14 @@ func initAnalysis(ctx context.Context, token string, accountId int) string {
 
 	accounts, err := client.Accounts(ctx)
 	if err != nil {
+		if err.Error() == "can't do request to" {
+			return "Сейчас есть проблемы на стороне брокера при получении счетов, попробуйте позже"
+		}
+		if err.Error() == "bad response to https://api-invest.tinkoff.ru/openapi/user/accounts code=401, body=" {
+			return "Неверный токен"
+		}
 		fmt.Errorf("Get account error: %s", err.Error())
-		return "Неверный токен"
+		return "Сейчас есть не совсем понятная проблема с получением счетов, попробуйте позже с другим токеном"
 	}
 	var userPortfolio Portfolio
 	var portfolio sdk.Portfolio
